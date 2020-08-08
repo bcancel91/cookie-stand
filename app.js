dda8139f47c1dbb5391332c729b06f270b86e2a7
Brian Cancel <brian.e.cancel@gmail.com>
2020-08-08 12:56:04 -0400

    initial push

---
+++ app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12am",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm"
+];
+
+var myObject1 = {
+  location: "seattle",
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  sales: []
+};
+
+var myObject2 = {
+  location: "tokyo",
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
+  sales: []
+};
+
+var myObject3 = {
+  location: "dubai",
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7,
+  sales: []
+};
+
+var myObject4 = {
+  location: "paris",
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3,
+  sales: []
+};
+
+var myObject5 = {
+  location: "lima",
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6,
+  sales: []
+};
